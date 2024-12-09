cbf35b46a4386398864c466acae712143dc1fd30
Er Xuanhe <exhere519@gmail.com>
2024-12-10 01:09:07 +0800

    second commit. Many changes.

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,47 +2,71 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   "/",
-  "/demo/",
+  // "/demo/",
+  // "/ai/",
   {
-    text: "随想",
-    icon: "pen-to-square",
-    prefix: "/posts/",
+    text: "AI",
+    icon: "robot",
+    prefix: "/ai/",
     children: [
-      {
-        text: "苹果",
-        icon: "pen-to-square",
-        prefix: "apple/",
-        children: [
-          { text: "苹果1", icon: "pen-to-square", link: "1" },
-          { text: "苹果2", icon: "pen-to-square", link: "2" },
-          "3",
-          "4",
-        ],
-      },
-      {
-        text: "香蕉",
-        icon: "pen-to-square",
-        prefix: "banana/",
-        children: [
-          {
-            text: "香蕉 1",
-            icon: "pen-to-square",
-            link: "1",
-          },
-          {
-            text: "香蕉 2",
-            icon: "pen-to-square",
-            link: "2",
-          },
-          "3",
-          "4",
-        ],
-      },
-      { text: "樱桃", icon: "pen-to-square", link: "cherry" },
-      { text: "火龙果", icon: "pen-to-square", link: "dragonfruit" },
-      "tomato",
-      "strawberry",
+      { text: "AI简史", icon: "clock-rotate-left", link: "AI_Concise_History" },
     ],
+  },
+  {
+    text: "Java后端",
+    icon: "robot",
+    prefix: "/javabackend/",
+    children: [
+      // { text: "Java简史", icon: "clock-rotate-left", link: "Java_Concise_History" },
+    ],
+  },
+
+  // {
+  //   text: "随想",
+  //   icon: "pen-to-square",
+  //   prefix: "/posts/",
+  //   children: [
+  //     {
+  //       text: "苹果",
+  //       icon: "pen-to-square",
+  //       prefix: "apple/",
+  //       children: [
+  //         { text: "苹果1", icon: "pen-to-square", link: "1" },
+  //         { text: "苹果2", icon: "pen-to-square", link: "2" },
+  //         "3",
+  //         "4",
+  //       ],
+  //     },
+  //     {
+  //       text: "香蕉",
+  //       icon: "pen-to-square",
+  //       prefix: "banana/",
+  //       children: [
+  //         {
+  //           text: "香蕉 1",
+  //           icon: "pen-to-square",
+  //           link: "1",
+  //         },
+  //         {
+  //           text: "香蕉 2",
+  //           icon: "pen-to-square",
+  //           link: "2",
+  //         },
+  //         "3",
+  //         "4",
+  //       ],
+  //     },
+  //     { text: "樱桃", icon: "pen-to-square", link: "cherry" },
+  //     { text: "火龙果", icon: "pen-to-square", link: "dragonfruit" },
+  //     "tomato",
+  //     "strawberry",
+  //   ],
+  // },
+
+  {
+    text: "个人介绍",
+    icon: "person",
+    link: "/intro",
   },
   // {
   //   text: "V2 文档",
--- src/README.md
+++ src/README.md
@@ -6,7 +6,7 @@ title: 主页
 heroImage: https://theme-hope-assets.vuejs.press/logo.svg
 heroText: Edward's Blog
 heroFullScreen: true
-tagline: 求知若渴，虚心若愚
+tagline: 求知若饥，虚心若愚
 projects:
   - icon: project
     name: 项目名称
@@ -41,8 +41,8 @@ projects:
 footer: 自定义你的页脚文字
 ---
 
-这是一个博客主页的案例。
+<!-- 这是一个博客主页的案例。
 
 要使用此布局，你应该在页面前端设置 `layout: BlogHome` 和 `home: true`。
 
-相关配置文档请见 [博客主页](https://theme-hope.vuejs.press/zh/guide/blog/home.html)。
+相关配置文档请见 [博客主页](https://theme-hope.vuejs.press/zh/guide/blog/home.html)。 -->
